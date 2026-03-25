a4568b89891ab6f53f1eae5b53a91008432946d5
juan071825 <juanmateoalvarez@gmail.com>
2026-03-25 22:58:28 +0100

    feat(Hole.java): :sparkles: Añadir nueva clase Hoyo.

---
+++ src/main/java/edu/teamrocket/golfstrokes/Hole.java
@@ -0,0 +1,33 @@
+package edu.teamrocket.golfstrokes;
+
+public class Hole {
+    
+    private final Byte number;
+    private final Byte par;
+
+    Hole(Byte number, Byte par){
+        this.number = number;
+        this.par = par;
+    }
+
+
+    Byte getNumber(){
+        return this.number;
+    }
+
+    Byte getPar(){
+        return this.par;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder hoyo = new StringBuilder();
+        hoyo.append("Par: ");
+        hoyo.append(this.number);
+        hoyo.append("  ");
+        hoyo.append("Number: ");
+        hoyo.append(this.par);
+        String stringHoyo = hoyo.toString();
+        return stringHoyo;
+    }
+}
